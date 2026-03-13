cb4d77dc6ccb88ebc78846b8fe523e4d5aec26dd
AregShahbazian <areg.uvagm@gmail.com>
2026-03-13 18:39:56 +0400

    break-even overlay fixed

--- .storybook/overlay-stories/overlays/break-even.ts
+++ .storybook/overlay-stories/overlays/break-even.ts
@@ -4,18 +4,17 @@ import {createPriceLine, type PriceLine} from "@superchart/index"
 export function createBreakEven(chart: Chart, price: number, color = "#D05DDF"): PriceLine {
   return createPriceLine(chart, {price})
     .setLineColor(color)
-    .setLineStyle("dashed")
+    .setLineStyle("solid")
     .setLineWidth(1)
-    .setLabelTextColor("#FFFFFF")
-    .setLabelBackgroundColor(color)
+    .setLabelTextColor(color)
+    .setLabelBackgroundColor("transparent")
     .setLabelBorderColor(color)
     .setYAxisLabelTextColor("#FFFFFF")
     .setYAxisLabelBackgroundColor(color)
     .setYAxisLabelBorderColor(color)
     .setLabelVisible(true)
-    .setLabelAlign('center')
-    .setLabelOffsetPercentX(50)
-    .setLabelPosition('center')
+    .setLabelAlign('right')
+    .setLabelPosition('below')
     .setEditable(true)
     .setText('Break Even')
 }
@@ -23,11 +22,6 @@ export function createBreakEven(chart: Chart, price: number, color = "#D05DDF"):
 export function updateBreakEven(line: PriceLine, price: number, color = "#D05DDF"): void {
   line
     .setPrice(price)
-    .setLineColor(color)
-    .setLabelBackgroundColor(color)
-    .setLabelBorderColor(color)
-    .setYAxisLabelBackgroundColor(color)
-    .setYAxisLabelBorderColor(color)
 }
 
 export function removeBreakEven(line: PriceLine): void {
